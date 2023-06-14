3833fa3f8ba77c3007926a7475c70ded82fa3698
feyselmubarek <87311061+sam5235@users.noreply.github.com>
2023-06-14 01:34:59 +0100

    admin clean-up

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function Home() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push("/patients");
+  }, []);
   return (
     <div className={styles.container}>
       <h1>Logged In</h1>
